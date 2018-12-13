c08c97db0039dba5a7630dcbb311ba270bb4d36e
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-13 19:44:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  
+  count = number
+  
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(integer)
+  second_count = integer
+  
+  while second_count > 0
+    puts "#{second_count} SECOND(S)!"
+    sleep(1)
+  second_count -= 1
+  end
+  
+  "HAPPY NEW YEAR!"
 end
